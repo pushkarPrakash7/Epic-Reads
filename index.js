a1d65d3402af7b1b8e765049a68df2e27ee514b5
ashp79 <112823223+ashp79@users.noreply.github.com>
2024-07-19 14:58:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,109 @@
+const express = require("express");
+const app = express();
+const port = process.env.port || 5000;
+const cors = require("cors");
+require('dotenv').config()
+
+const { ObjectId } = require("mongodb");
+
+app.use(cors());
+app.use(express.json());
+
+const path = require("path");
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGO_URL;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    
+    await client.connect();
+
+    
+    const booksCollections = client.db("BookInventory").collection("books");
+
+
+    app.post("/upload-book", async (req, res) => {
+      const data = req.body;
+      const result = await booksCollections.insertOne(data);
+      res.send(result);
+    });
+
+    
+    app.get("/all-books",async(req, res)=>{
+        const books =  booksCollections.find();
+        const result = await books.toArray();
+        res.send(result); 
+    })
+
+    
+    app.patch("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      
+      const updateBookData = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+
+      const updateDoc = {
+        $set: { ...updateBookData },
+      };
+
+      const result = await booksCollections.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.send(result);
+    });
+
+    
+    app.delete("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await booksCollections.deleteOne(filter);
+      res.send(result);
+    });
+
+    
+    app.get("/all-books", async (req, res) => {
+      let query = {};
+      if (req.query.category) {
+        query = { Category: req.query.category };
+      }
+      const result = await booksCollections.find(query).toArray();
+      res.send(result);
+    });
+
+    
+    app.get("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await booksCollections.findOne(filter);
+      res.send(result);
+    });
+
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
